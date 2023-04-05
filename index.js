013e5c7a7c07d576c82db107556ae507b02f4432
wael kamira <ramondm4@gmail.com>
2023-04-06 00:42:30 +0300

    Assignment Completed

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide = (a , b) => {
+    return a / b;
+};
+console.log(divide(2000,100));
+
+const square = (x) => {
+    return x * x;
+};
+
+console.log(square(2))
+
+const add = (a,b) => {
+    return a + b;
+};
+
+console.log(add(5,2))
